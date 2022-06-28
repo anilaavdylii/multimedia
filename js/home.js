8feb7c7c94ad98c88396a45967ce75eb2e9a2932
anilaavdylii <aa48857@ubt-uni.net>
2022-06-29 01:07:07 +0200

    banner adjustment

--- js/home.js
+++ js/home.js
@@ -166,12 +166,3 @@ window.onload = function() {
 }
 
 
-
-var myVideo = document.getElementById("video1"); 
-
-function playPause() { 
-  if (myVideo.paused) 
-    myVideo.play(); 
-  else 
-    myVideo.pause(); 
-} 
