948d6ea1462296f1cb6416764ffa3412a43a59ba
anilaavdylii <aa48857@ubt-uni.net>
2022-07-27 01:20:56 +0200

    added the opportunity to move a text only when scrolling in country pages

--- js/cursor.js
+++ js/cursor.js
@@ -32,3 +32,11 @@ counters.forEach((counter) => {
   updateCounter();
 });
 
+
+
+let text = document.querySelector('.banner h2');
+
+window.addEventListener('scroll', () => {
+  let value = window.scrollY;
+  text.style.marginBottom = value * 2 + 'px';
+})
